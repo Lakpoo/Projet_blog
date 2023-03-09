64d6dc96a448d797ff5ffa7a989ad288b7e2319e
lakpoo <kiliandominik93@gmail.com>
2023-03-09 14:37:31 +0100

    Resolution securite page admin

--- auth/adminCheck.php
+++ auth/adminCheck.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-if (!$_SESSION['degre_privilege'] == 1) {
+if ($_SESSION['degre_privilege'] == 1) {
+    header('location: ../pages/admin.php');
+}else{
     header('location: ../pages/index.php');
 }
-else{
-    header('location: ../pages/admin.php');
-}
+
 ?>

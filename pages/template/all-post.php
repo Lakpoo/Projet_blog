f372ef0c95cd1c96697e394e3851f72b448831f3
lakpoo <kiliandominik93@gmail.com>
2023-03-12 02:58:10 +0100

    css

--- pages/template/all-post.php
+++ pages/template/all-post.php
@@ -24,7 +24,12 @@ try {
             <div class="footer-post">
                 <div class="auteur">
                     <p>Writter by : <?php echo $article['auteur'] ?></p>
-                    
+                </div>
+                <div class="commentaire">
+                    <form action="/pages/commentaire/comment-post.php">
+                        <input type="text" name="id_post" value="<?= $article['id_post'] ?>" hidden>
+                        <button>Commenter</button>
+                    </form>
                 </div>
                 <div class="time">
                     <?php echo gmdate("d-m-Y H:i:s", ($article['log'] + 3600));?>
